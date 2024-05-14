7a507d19d61a3176eb42888e240b320fe82168b3
FranciscoJSB12 <javif8@gmail.com>
2024-05-14 16:29:51 -0400

    refactor: Implement home header componet for better organization

--- src/presentation/components/Home/HomeHeader.tsx
+++ src/presentation/components/Home/HomeHeader.tsx
@@ -1,5 +1,42 @@
-import { View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { CategoryBar } from './CategoryBar';
 
 export const HomeHeader = () => {
-  return <View></View>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.mainTitle}>Mi lunchera</Text>
+      <Text style={styles.subTitle}>Menú del día</Text>
+      <MaterialCommunityIcons
+        name='store-search-outline'
+        size={36}
+        color='#172554'
+        style={styles.iconStyle}
+      />
+      <CategoryBar />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    paddingHorizontal: 28,
+  },
+  mainTitle: {
+    fontSize: 32,
+    fontWeight: '800',
+    color: '#172554',
+    fontStyle: 'italic',
+  },
+  subTitle: {
+    fontSize: 28,
+    fontWeight: '800',
+    color: '#172554',
+  },
+  iconStyle: {
+    position: 'absolute',
+    top: 10,
+    right: 28,
+  },
+});
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,13 +1,12 @@
 import { useEffect, useState } from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import { ProductsAPIResponse } from '../../../infrastucture/interfaces/mi-lunchera-products.response';
 import { productsApi } from '../../../config/api/productsApi';
+import { HomeHeader } from '../../components/Home/HomeHeader';
 import { CardImage } from '../../components/Home/CardImage';
 import { CardCaption } from '../../components/Home/CardCaption';
 import { Card } from '../../components/Home/Card';
-import { CategoryBar } from '../../components/Home/CategoryBar';
 
 export const HomeScreen = () => {
   const [products, setProducts] = useState<ProductsAPIResponse[]>([]);
@@ -25,51 +24,14 @@ export const HomeScreen = () => {
 
   return (
     <View
-      style={{
-        flex: 1,
-        paddingTop: top + 20,
-        alignItems: 'center',
-        paddingBottom: 40,
-        backgroundColor: '#f3f4f6',
-      }}
+      style={[
+        {
+          paddingTop: top + 20,
+        },
+        styles.container,
+      ]}
     >
-      <View
-        style={{
-          width: '100%',
-          paddingHorizontal: 28,
-        }}
-      >
-        <Text
-          style={{
-            fontSize: 32,
-            fontWeight: '800',
-            color: '#172554',
-            fontStyle: 'italic',
-          }}
-        >
-          Mi lunchera
-        </Text>
-        <Text
-          style={{
-            fontSize: 28,
-            fontWeight: '800',
-            color: '#172554',
-          }}
-        >
-          Menú del día
-        </Text>
-        <MaterialCommunityIcons
-          name='store-search-outline'
-          size={36}
-          color='#172554'
-          style={{
-            position: 'absolute',
-            top: 10,
-            right: 28,
-          }}
-        />
-        <CategoryBar />
-      </View>
+      <HomeHeader />
       {products.length > 0 ? (
         <FlatList
           showsVerticalScrollIndicator={false}
@@ -87,8 +49,17 @@ export const HomeScreen = () => {
           keyExtractor={item => item.id}
         />
       ) : (
-        <Text>Loading</Text>
+        <Text>Loading...</Text>
       )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    paddingBottom: 40,
+    backgroundColor: '#f3f4f6',
+  },
+});
